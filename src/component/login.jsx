91282fa32e1024b71dcaa80f9fb0f69d14df5bd5
Jeneliya12 <gurungjeneliya@gmail.com>
2024-08-29 16:47:47 -0400

    connect login api with backend

--- src/component/login.jsx
+++ src/component/login.jsx
@@ -1,11 +1,31 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { base_url } from "../utils/base-url";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(`${base_url}user/login`, {
+        email,
+        password,
+      });
+      console.log("Login successful:", response.data);
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
+
   return (
     <>
-      {/* Login Form Section */}
       <div className="flex justify-center items-center min-h-screen bg-gray-100">
         <div className="w-full max-w-md p-8 bg-white rounded-xl shadow-lg -mt-16">
           <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="mb-4">
               <label
                 className="block text-gray-700 font-semibold mb-2"
@@ -16,6 +36,8 @@ function Login() {
               <input
                 type="email"
                 id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 placeholder="Enter your email"
                 className="w-full px-4 py-2 border rounded-xl text-gray-800"
               />
@@ -30,6 +52,8 @@ function Login() {
               <input
                 type="password"
                 id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 placeholder="Enter your password"
                 className="w-full px-4 py-2 border rounded-xl text-gray-800"
               />
